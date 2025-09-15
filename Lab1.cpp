49b2c739b58a8156de605c3ee8ff29aee1c01639
eremeevaim <i.milyanova@vk.com>
2025-09-15 18:33:48 +0300

    new functions and menu

--- Lab1.cpp
+++ Lab1.cpp
@@ -1,10 +1,9 @@
-﻿#include<iostream>
-#include<fstream>
-#include<string>
-
+﻿#include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
 
-struct Pipe{
+struct Pipe {
 	string name;
 	float length;
 	int diametr;
@@ -17,12 +16,52 @@ struct KS {
 	int KCV;
 };
 
+
 int main() {
 	setlocale(LC_ALL, "RU");
-	int a;
-	Pipe truba1;
-	truba1.name == "Uchastok 1 km 12";
-	cout << truba1.name;
 
+	Pipe truba;
+	KS station;
+	int chosen = 0;
+
+	while (true) { 
+		cout << "МЕНЮ КНОПОК: '1' - Добавить трубу, '2' - Добавить КС , '3' -  Просмотр всех объектов, '4' - Редактировать трубу, '5' - Редактировать КС, '6' -  Сохранить, '7' - Загрузить, '0' - Выход. " << endl;
+		cin >> chosen;
+		if (chosen == 1) {
+			cout << "Ввведите характеристики для трубы." << endl;
+			cout << "Название: ";
+			cin >> truba.name;
+
+			cout << "Длина (км): ";
+			cin >> truba.length;
+
+			cout << "Диаметр: ";
+			cin >> truba.diametr;
+
+			cout << "В ремонте ли труба? (Да - '1', Нет - '0') ";
+			cin >> truba.check;
+		}
+
+		else if (chosen == 2) {
+			
+			cout << "Ввведите характеристики для станции." << endl;
+			cout << "Название: ";
+			cin >> station.name2;
+
+			cout << "Количество цехов: ";
+			cin >> station.KC;
+
+			cout << "Количество цехов (в работе): ";
+			cin >> station.KCV;
+		}
+
+		
+		else if (chosen == 0) {
+			cout << "Выход из программы." << endl;
+			break;
+		}
+	}
+	
 	return 0;
 }
+
