5600b5455078eb334e341f784b3d9ab8da605827
eremeevaim <i.milyanova@vk.com>
2025-09-15 20:28:28 +0300

    the logic of the "view all objects" button has been changed

--- Lab1.cpp
+++ Lab1.cpp
@@ -8,12 +8,14 @@ struct Pipe {
 	float length;
 	int diametr;
 	bool check;
+	bool isAdded = false;
 };
 
 struct KS {
 	string name2;
 	int KC;
 	int KCV;
+	bool isAdded = false;
 };
 
 
@@ -40,11 +42,14 @@ int main() {
 
 			cout << "В ремонте ли труба? (Да - '1', Нет - '0') ";
 			cin >> truba.check;
+
+			truba.isAdded = true;
+			cout << "Данные трубы успешно добавлены." << endl;
 		}
 
 		else if (chosen == 2) {
 			
-			cout << "Ввведите характеристики для станции." << endl;
+			cout << "Введите характеристики для станции." << endl;
 			cout << "Название: ";
 			cin >> station.name2;
 
@@ -53,15 +58,38 @@ int main() {
 
 			cout << "Количество цехов (в работе): ";
 			cin >> station.KCV;
-		}
 
-		
+			station.isAdded = true;
+			cout << "Данные КС успешно добавлены." << endl;
+		}
+		else if (chosen == 3) {
+			bool bothAdded = false;
+			if (truba.isAdded) {
+				cout << "ХАРАКАТЕРИСТИКИ ТРУБЫ" << endl;
+				cout << "Название: " << truba.name << endl;
+				cout << "Длина (км): " << truba.length << endl;
+				cout << "Диаметр: " << truba.diametr << endl;
+				cout << "В ремонте ли труба? (Да - '1', Нет - '0') " << truba.check << endl;
+				bothAdded = true;
+			}
+			if (station.isAdded) {
+				cout << "ХАРАКТЕРИСТИКИ КС" << endl;
+				cout << "Название: " << station.name2 << endl;
+				cout << "Количество цехов: " << station.KC << endl;
+				cout << "Количество цехов (в работе): " << station.KCV << endl;
+				bothAdded = true;
+			}
+
+			if (!bothAdded) {
+				cout << "Никаких данных не было введено." << endl;
+			}
+		}
 		else if (chosen == 0) {
 			cout << "Выход из программы." << endl;
 			break;
 		}
 	}
-	
+
 	return 0;
 }
 
