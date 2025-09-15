c5bf3321b71202dcdb2f1e1903289f6c913f9191
eremeevaim <i.milyanova@vk.com>
2025-09-15 20:52:55 +0300

    editing functions of "pipes" and "ks"

--- Lab1.cpp
+++ Lab1.cpp
@@ -84,6 +84,94 @@ int main() {
 				cout << "Никаких данных не было введено." << endl;
 			}
 		}
+
+		else if (chosen == 4) {
+			if (!truba.isAdded) {
+				cout << "Нет данных о трубе для редактирования. Сначала добавьте трубу." << endl;
+			}
+			else
+			{
+				cout << "Редактирование трубы. Текущие значения:" << endl;
+				cout << "1. Название: " << truba.name << endl;
+				cout << "2. Длина: " << truba.length << " км" << endl;
+				cout << "3. Диаметр: " << truba.diametr << endl;
+				cout << "4. Статус ремонта: " << (truba.check ? "В ремонте" : "Не в ремонте") << endl;
+
+				cout << "Что вы хотите изменить? (1-4, 0 - отмена): ";
+				int editChoice;
+				cin >> editChoice;
+				if (editChoice == 0) {
+					cout << "Редактирование отменено." << endl;
+				}
+				else if (editChoice == 1) {
+					cout << "Введите новое название: ";
+					cin >> truba.name;
+					cout << "Название обновлено." << endl;
+				}
+				else if (editChoice == 2) {
+					cout << "Введите новую длину (км): ";
+					cin >> truba.length;
+					cout << "Длина обновлена." << endl;
+				}
+				else if (editChoice == 3) {
+					cout << "Введите новый диаметр: ";
+					cin >> truba.diametr;
+					cout << "Диаметр обновлен." << endl;
+				}
+				else if (editChoice == 4) {
+					cout << "В ремонте ли труба? (Да - '1', Нет - '0') ";
+					cin >> truba.check;
+					cout << "Статус ремонта обновлен." << endl;
+				}
+				else {
+					cout << "Неверный выбор." << endl;
+				}
+			}
+		}
+		else if (chosen == 5) {
+			if (!station.isAdded) {
+				cout << "Нет данных о КС для редактирования. Сначала добавьте КС." << endl;
+			}
+			else {
+				cout << "Редактирование КС. Текущие значения:" << endl;
+				cout << "1. Название: " << station.name2 << endl;
+				cout << "2. Количество цехов: " << station.KC << endl;
+				cout << "3. Количество цехов в работе: " << station.KCV << endl;
+
+				cout << "Что вы хотите изменить? (1-3, 0 - отмена): ";
+				int editChoice;
+				cin >> editChoice;
+
+				if (editChoice == 0) {
+					cout << "Редактирование отменено." << endl;
+				}
+				else if (editChoice == 1) {
+					cout << "Введите новое название: ";
+					cin >> station.name2;
+					cout << "Название обновлено." << endl;
+				}
+				else if (editChoice == 2) {
+					cout << "Введите новое количество цехов: ";
+					cin >> station.KC;
+					cout << "Количество цехов обновлено." << endl;
+				}
+				else if (editChoice == 3) {
+					cout << "Введите новое количество цехов в работе: ";
+					cin >> station.KCV;
+
+					if (station.KCV > station.KC) {
+						cout << "Ошибка!Количество цехов в работе не может превышать общее количество цехов." << endl;
+						station.KCV = station.KC;
+					}
+					else {
+						cout << "Количество цехов в работе обновлено." << endl;
+					}
+				}
+				else {
+					cout << "Неверный выбор." << endl;
+				}
+			}
+		}
 		else if (chosen == 0) {
 			cout << "Выход из программы." << endl;
 			break;
